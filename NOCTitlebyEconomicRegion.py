fde45de0183cc1e99e4ea78dc112b634a79a9e5d
LucasSpielman <lucasalanspielman@gmail.com>
2025-03-09 18:28:58 -0600

    added better swapping between french and english

--- NOCTitlebyEconomicRegion.py
+++ NOCTitlebyEconomicRegion.py
@@ -80,12 +80,81 @@ def load_data(language):
 # Initialize the Dash app with the Minty theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
 
+# Define text content for both languages
+text_content = {
+    'English': {
+        'title': "Canadian Job Market Outlook 2024-2026",
+        'data_source': "Data sourced and provided by the Government of Canada.",
+        'visit_website': "Visit the website",
+        'open_data': "Open Canada Data",
+        'noc_title_placeholder': "Search for Job Title...",
+        'modal_info': """
+            This Dash app provides an overview of the Canadian job market outlook for 2024-2026. 
+            The data is sourced from the Government of Canada and provides insights into job outlooks 
+            across various economic regions. The outlook categories include 'very good', 'good', 
+            'moderate', 'limited', 'undetermined', and 'None'. 
+
+            **Features:**
+
+            - **Region Dropdown:** Select an economic region to view its job outlook.
+            - **NOC Title Search:** Search for specific job titles within the selected region.
+            - **Outlook Dropdown:** Filter job titles based on their outlook category.
+            - **Map Plot:** Visual representation of the selected region.
+            - **Bar Plot:** Displays job titles and their outlooks in the selected region.
+            - **Pagination:** Navigate through job titles using the page slider.
+            - **Language Selection:** Switch between English and French data.
+
+            **How to Use:**
+
+            1. Select a region from the 'Region Dropdown'.
+            2. Optionally, search for a specific job title using the 'NOC Title Search' input.
+            3. Filter the results by selecting an outlook category from the 'Outlook Dropdown'.
+            4. View the map and bar plots to analyze the job outlooks.
+            5. Click on a job title in the bar plot to view detailed employment trends.
+            6. Use the 'Page Slider' to navigate through multiple pages of job titles.
+            7. Switch the language using the 'Language Dropdown' to view data in English or French.
+        """
+    },
+    'French': {
+        'title': "Perspectives du marché du travail canadien 2024-2026",
+        'data_source': "Données fournies par le gouvernement du Canada.",
+        'visit_website': "Visitez le site web",
+        'open_data': "Données ouvertes du Canada",
+        'noc_title_placeholder': "Recherchez un titre d'emploi...",
+        'modal_info': """
+            Cette application Dash fournit un aperçu des perspectives du marché du travail canadien pour 2024-2026. 
+            Les données sont fournies par le gouvernement du Canada et offrent des informations sur les perspectives d'emploi 
+            dans diverses régions économiques. Les catégories de perspectives incluent 'très bonnes', 'bonnes', 
+            'modérées', 'limitées', 'indéterminées' et 'Aucune'. 
+
+            **Caractéristiques:**
+
+            - **Menu déroulant des régions:** Sélectionnez une région économique pour voir ses perspectives d'emploi.
+            - **Recherche de titre NOC:** Recherchez des titres d'emploi spécifiques dans la région sélectionnée.
+            - **Menu déroulant des perspectives:** Filtrez les titres d'emploi en fonction de leur catégorie de perspectives.
+            - **Carte:** Représentation visuelle de la région sélectionnée.
+            - **Graphique en barres:** Affiche les titres d'emploi et leurs perspectives dans la région sélectionnée.
+            - **Pagination:** Naviguez à travers les titres d'emploi en utilisant le curseur de page.
+            - **Sélection de la langue:** Passez des données en anglais aux données en français.
+
+            **Comment utiliser:**'
+            1. Sélectionnez une région dans le 'Menu déroulant des régions'.
+            2. Facultativement, recherchez un titre d'emploi spécifique en utilisant le champ de recherche 'Recherche de titre NOC'.
+            3. Filtrez les résultats en sélectionnant une catégorie de perspectives dans le 'Menu déroulant des perspectives'.
+            4. Consultez la carte et les graphiques en barres pour analyser les perspectives d'emploi.
+            5. Cliquez sur un titre d'emploi dans le graphique en barres pour voir les tendances d'emploi détaillées.
+            6. Utilisez le 'Curseur de page' pour naviguer à travers plusieurs pages de titres d'emploi.
+            7. Changez la langue en utilisant le 'Menu déroulant des langues' pour voir les données en anglais ou en français.
+        """
+    }
+}
+
 # Define the layout of the app
 app.layout = dbc.Container([
     # Title row
     dbc.Row([
-        dbc.Col(html.H1("Canadian Job Market Outlook 2024-2026", style={'textAlign': 'Left'}), width=10),
-        dbc.Col(dbc.Button("Info", id="open-modal", color="primary"), width=2, style={'textAlign': 'right'})
+        dbc.Col(html.H1(id='app-title', style={'textAlign': 'Left', 'margin-top': '30px'}), width=10),
+        dbc.Col(dbc.Button("Info", id="open-modal", color="primary"), width=2, style={'textAlign': 'right', 'margin-top': '15px'})
     ]),
     
     # Region dropdown row
@@ -144,9 +213,11 @@ app.layout = dbc.Container([
     # Data source row
     dbc.Row([
         dbc.Col(html.Div([
-            html.P("Data sourced and provided by the Government of Canada."),
-            html.A("Visit the website", href="https://www.statcan.gc.ca/en/subjects/standard/noc/2021/indexV1", target="_blank"),
-            html.A("Open Canada Data", href="https://open.canada.ca/data/en/dataset/b0e112e9-cf53-4e79-8838-23cd98debe5b?_gl=1*h2x1ic*_ga*MTc4Mjg5MzYwMi4xNjc4MTQ5Mjc1*_ga_S9JG8CZVYZ*MTczNDM4ODMyOC4xMi4xLjE3MzQzODg2OTUuNDkuMC4w", target="_blank")
+            html.P(id='data-source-text'),
+            html.Br(),
+            html.A(id='visit-website-link', href="https://www.statcan.gc.ca/en/subjects/standard/noc/2021/indexV1", target="_blank"),
+            html.Br(),
+            html.A(id='open-data-link', href="https://open.canada.ca/data/en/dataset/b0e112e9-cf53-4e79-8838-23cd98debe5b?_gl=1*h2x1ic*_ga*MTc4Mjg5MzYwMi4xNjc4MTQ5Mjc1*_ga_S9JG8CZVYZ*MTczNDM4ODMyOC4xMi4xLjE3MzQzODg2OTUuNDkuMC4w", target="_blank")
         ], style={'text-align': 'center', 'margin-top': '20px'}), width=12)
     ]),
     
@@ -167,32 +238,7 @@ app.layout = dbc.Container([
         dbc.Modal(
             [
                 dbc.ModalHeader(dbc.ModalTitle("About This App")),
-                dbc.ModalBody(
-                """
-                This Dash app provides an overview of the Canadian job market outlook for 2024-2026. 
-                The data is sourced from the Government of Canada and provides insights into job outlooks 
-                across various economic regions. The outlook categories include 'very good', 'good', 
-                'moderate', 'limited', 'undetermined', and 'None'. 
-
-                **Features:**
-                - **Region Dropdown:** Select an economic region to view its job outlook.
-                - **NOC Title Search:** Search for specific job titles within the selected region.
-                - **Outlook Dropdown:** Filter job titles based on their outlook category.
-                - **Map Plot:** Visual representation of the selected region.
-                - **Bar Plot:** Displays job titles and their outlooks in the selected region.
-                - **Pagination:** Navigate through job titles using the page slider.
-                - **Language Selection:** Switch between English and French data.
-
-                **How to Use:**
-                1. Select a region from the 'Region Dropdown'.
-                2. Optionally, search for a specific job title using the 'NOC Title Search' input.
-                3. Filter the results by selecting an outlook category from the 'Outlook Dropdown'.
-                4. View the map and bar plots to analyze the job outlooks.
-                5. Click on a job title in the bar plot to view detailed employment trends.
-                6. Use the 'Page Slider' to navigate through multiple pages of job titles.
-                7. Switch the language using the 'Language Dropdown' to view data in English or French.
-                """
-                ),
+                dbc.ModalBody(dcc.Markdown(id='modal-info-text')),
                 dbc.ModalFooter(
                     dbc.Button("Close", id="close-modal", className="ms-auto", n_clicks=0)
                 ),
@@ -219,6 +265,20 @@ app.layout = dbc.Container([
         style={"maxWidth": "100%"}  # Adjust the width of the modal
     ),
 ], fluid=True)
+
+# Callback to update text content based on selected language
+@app.callback(
+    [Output('app-title', 'children'),
+     Output('data-source-text', 'children'),
+     Output('visit-website-link', 'children'),
+     Output('open-data-link', 'children'),
+     Output('modal-info-text', 'children'),
+     Output('noc-title-input', 'placeholder')],
+    Input('language-dropdown', 'value')
+)
+def update_text_content(language):
+    content = text_content[language]
+    return (content['title'], content['data_source'], content['visit_website'], content['open_data'], content['modal_info'], content['noc_title_placeholder'])
 
 # Callback to update dropdown options based on selected language
 @app.callback(
