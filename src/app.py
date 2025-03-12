6598fe618d2a5d6991523777aa8f4e60942fe297
LucasSpielman <lucasalanspielman@gmail.com>
2025-03-12 16:52:24 -0600

    removed data source row

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ import dash
 from dash import html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
 
-from ERbyNOCTitle import create_data_source_row
 from text_content import text_content_noc
 
 # Import your two existing Dash app layouts and callbacks
@@ -34,8 +33,7 @@ main_layout = html.Div([
         dcc.Tab(label='Canadian Job Market Outlook', value='tab1'),
         dcc.Tab(label='Canadian Economic Region Outlook', value='tab2')
     ]),
-    html.Div(id='tabs-content'),
-    create_data_source_row()  # Add the data source row here
+    html.Div(id='tabs-content')
 ])
 
 # Define the callback to switch tabs
